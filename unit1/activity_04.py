c9a3338676a42f5ad448bb2b1ac98668c7d18273
PabloPolMartin <pablomartin8699@hotmail.com>
2021-05-21 08:45:51 -0500

    intento

---
+++ unit1/activity_04.py
@@ -0,0 +1,15 @@
+###### DECLARE VARIABALES ######
+
+num1 = 0 
+num2 = 0
+result_add = 0
+result_m = 0
+
+num1 = int ( input( " Enter number1 " ) )
+num2 = int ( input( " Enter number2 " ) )
+
+
+result_add = num1 + num2
+result_m = num1 * num2 
+print(" this is the add: ", result_add )
+print(" this is the m: ", result_m )
