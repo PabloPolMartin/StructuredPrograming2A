38f80f703f7efe33fd89d640ab0bf47697bbff5c
PabloPolMartin <pablomartin8699@hotmail.com>
2021-07-22 13:05:20 -0500

    unit2

---
+++ unit2/practica2.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == "__main__":
+    print("Practica2 CodeBots")
+    edificio = [0,1,1,2],
+               [0,5,0,0],
+               [2,0,3,3]
+    print( listElementsSum( edificio))
+
+    # for i in range(0, len(eidificio[0])  ):
+    #         for j in range(0, len( edificio) ): #for anidado
+    #                 print( f" edificio[{i}]:   {edificio[j][i]}")
